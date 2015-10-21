322c4ed715aa3bb5051af020f01f25a1db4100fa
beefsupreme7 <niklas.debert@student.fh-kiel.de>
2015-10-21 16:47:28 +0200

    hashtags weg

--- kraken_slave.py
+++ kraken_slave.py
@@ -6,27 +6,39 @@ uploadURL = 'http://www.blog.pythonlibrary.org/wp-content/uploads/2012/06/wxDbVi
 id = 0
 status = 'open'
 
-def getJobs(url):
+
+def get_jobs(url):
     j = requests.get(url)
     # status = blocked
     jobList = j.json()
     return jobList
 
-# file downloaden
-def getFile(url):
-    if not os.path.exists('DATA/downloadedzip'):
-        os.makedirs('DATA/downloadedzip')
 
-    r = requests.get(url, stream=True)
-    with open('DATA/downloadedzip/code2.zip', 'wb') as f:
+# file downloaden
+def download_file(srcpath, filename, dstpath):
+    if not os.path.exists(dstpath):
+        os.makedirs(dstpath)
+
+    r = requests.get(srcpath, stream=True)
+    with open(dstpath + '/' + filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk:
                     f.write(chunk)
+
+# file uploaden
+
+def upload_file(srcpath, filename, dstpath):
+    files = {'upload_file': open(srcpath + '/' + filename,'rb')}
+    values = {'DB': 'photcat', 'OUT': 'csv', 'SHORT': 'short'}
+
+    r = requests.post(dstpath, files=files, data=values)
+
 
 # zip Archiv entpacken
 def unzip(srcpath, zipname, dstpath):
     if not os.path.exists(srcpath):
         os.makedirs(srcpath)
+
     zfile = zipfile.ZipFile(srcpath + zipname)
     for name in zfile.namelist():
         (dirname, filename) = os.path.split(name)
@@ -34,10 +46,12 @@ def unzip(srcpath, zipname, dstpath):
 
         zfile.extract(name, dstpath)
 
+
 # zip Archiv erstellen
 def zip(srcpath, zipname, dstpath):
     if not os.path.exists(dstpath):
         os.makedirs(dstpath)
+
     zfile = zipfile.ZipFile(dstpath + zipname, 'w', zipfile.ZIP_DEFLATED)
     abs_src = os.path.abspath(srcpath)
     for dirname, subdirs, files in os.walk(srcpath):
@@ -47,16 +61,3 @@ def zip(srcpath, zipname, dstpath):
             print('ZIP: ' + srcpath + filename + ' to ' + dstpath + zipname)
             zfile.write(absname, arcname)
     zfile.close()
-
-getFile(downloadURL)
-
-# Archiv entpacken aus /DATA/downloadedzip in /DATA/extractedzip
-unzip('DATA/downloadedzip/', 'code2.zip', 'DATA/extractedzip/')
-print('')
-
-# Archiv erstellen aus /DATA/extractedzip in /DATA/resultzip
-zip('DATA/extractedzip/', 'result.zip', 'DATA/resultzip/')
-print('')
-
-jobList = getJobs(jobsURL)
-print(jobList['message'])
